8d937047946ad6335933e64c6d9dc87e5ada73d6
abindal6@gatech.edu <abindal6@gatech.edu>
2026-06-17 14:59:08 -0700

    Updated component to use

--- src/components/ti-option3/TIDifferences/TIDifferencesOption3.jsx
+++ src/components/ti-option3/TIDifferences/TIDifferencesOption3.jsx
@@ -1,13 +1,17 @@
 import s from './TIDifferencesOption3.module.css'
 import { TEXT_REDS, TEXT_GREENS } from './geometry.js'
 import { Intro } from './Intro.jsx'
-import { CollapsingDiscs4 } from './CollapsingDiscs4.jsx'
+import { CollapsingDiscs3 } from "./CollapsingDiscs3.jsx";
 
 export default function TIDifferencesOption3() {
   return (
-    <div id="section-ti-differences" className={s.root} style={{ '--red': TEXT_REDS, '--green': TEXT_GREENS }}>
+    <div
+      id="section-ti-differences"
+      className={s.root}
+      style={{ "--red": TEXT_REDS, "--green": TEXT_GREENS }}
+    >
       <Intro />
-      <CollapsingDiscs4 />
+      <CollapsingDiscs3 />
     </div>
-  )
+  );
 }
