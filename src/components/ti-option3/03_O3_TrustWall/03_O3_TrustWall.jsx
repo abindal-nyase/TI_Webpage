0791d8ae3832679ff855db7b26736344f1f431a2
abindal6@gatech.edu <abindal6@gatech.edu>
2026-06-19 14:18:31 -0700

    Reorg

--- src/components/ti-option3/03_O3_TrustWall/03_O3_TrustWall.jsx
+++ src/components/ti-option3/03_O3_TrustWall/03_O3_TrustWall.jsx
@@ -10,56 +10,89 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from './03_O3_TrustWall.module.css';
 
+import aecom from '../../../assets/company-logos/aecom.svg?url'
+import area from '../../../assets/company-logos/area.webp?url'
+import asdsky from '../../../assets/company-logos/asdsky.webp?url'
+import brookfieldProperties from '../../../assets/company-logos/brookfield-properties.svg?url'
+import cbre from '../../../assets/company-logos/cbre.svg?url'
+import cim from '../../../assets/company-logos/cim.webp?url'
+import clune from '../../../assets/company-logos/clune.webp?url'
+import cushmanWakefield from '../../../assets/company-logos/cushman-wakefield.svg?url'
+import dlrGroup from '../../../assets/company-logos/dlr-group.webp?url'
+import form from '../../../assets/company-logos/form.webp?url'
+import gensler from '../../../assets/company-logos/gensler.svg?url'
+import gruen from '../../../assets/company-logos/gruen.webp?url'
+import hed from '../../../assets/company-logos/hed.webp?url'
+import hines from '../../../assets/company-logos/hines.webp?url'
+import hok from '../../../assets/company-logos/hok.webp?url'
+import huntsman from '../../../assets/company-logos/huntsman.webp?url'
+import irvineCompany from '../../../assets/company-logos/irvine-company.webp?url'
+import johnsonFain from '../../../assets/company-logos/johnson-fain.webp?url'
+import kda from '../../../assets/company-logos/kda.webp?url'
+import lpaInc from '../../../assets/company-logos/lpa-inc.webp?url'
+import mataConstruction from '../../../assets/company-logos/mata-construction.webp?url'
+import novo from '../../../assets/company-logos/novo.webp?url'
+import perkinsWill from '../../../assets/company-logos/perkins-will.svg?url'
+import rios from '../../../assets/company-logos/rios.webp?url'
+import rising from '../../../assets/company-logos/rising.webp?url'
+import shlemmerKamusAlgaze from '../../../assets/company-logos/shlemmer-kamus-algaze.webp?url'
+import som from '../../../assets/company-logos/som.webp?url'
+import studio111 from '../../../assets/company-logos/studio111.webp?url'
+import swinerton from '../../../assets/company-logos/swinerton.webp?url'
+import tismanSpeyer from '../../../assets/company-logos/tishman-speyer.webp?url'
+import turnerTownsend from '../../../assets/company-logos/turner-townsend.webp?url'
+import wareMalcomb from '../../../assets/company-logos/ware-malcomb.webp?url'
+
 gsap.registerPlugin(ScrollTrigger);
 
 const ROW_TOP = [
-  { id: 1, name: "Clune Construction", src: "/logos/clune.webp" },
-  { id: 2, name: "Mata Construction", src: "/logos/mata-construction.webp" },
-  { id: 3, name: "Novo Construction", src: "/logos/novo.webp" },
-  { id: 4, name: "Swinerton", src: "/logos/swinerton.webp" },
-  { id: 5, name: "Turner & Townsend", src: "/logos/turner-townsend.webp" },
+  { id: 1, name: "Clune Construction", src: clune },
+  { id: 2, name: "Mata Construction", src: mataConstruction },
+  { id: 3, name: "Novo Construction", src: novo },
+  { id: 4, name: "Swinerton", src: swinerton },
+  { id: 5, name: "Turner & Townsend", src: turnerTownsend },
   {
     id: 6,
     name: "Brookfield Properties",
-    src: "/logos/brookfield-properties.svg",
+    src: brookfieldProperties,
   },
-  { id: 7, name: "CBRE", src: "/logos/cbre.svg" }, //ok
-  { id: 8, name: "CIM", src: "/logos/cim.webp" },
-  { id: 9, name: "Cushman & Wakefield", src: "/logos/cushman-wakefield.svg" },
-  { id: 10, name: "Hines", src: "/logos/hines.webp" },
-  { id: 11, name: "Irvine Company", src: "/logos/irvine-company.webp" },
+  { id: 7, name: "CBRE", src: cbre },
+  { id: 8, name: "CIM", src: cim },
+  { id: 9, name: "Cushman & Wakefield", src: cushmanWakefield },
+  { id: 10, name: "Hines", src: hines },
+  { id: 11, name: "Irvine Company", src: irvineCompany },
   {
     id: 12,
     name: "Rising Realty Partners",
-    src: "/logos/rising.webp",
+    src: rising,
   },
-  { id: 13, name: "Tishman Speyer", src: "/logos/tishman-speyer.webp" },
-  { id: 14, name: "AECOM", src: "/logos/aecom.svg" },
-  { id: 15, name: "ASDSKY", src: "/logos/asdsky.webp" },
-  { id: 16, name: "Gensler", src: "/logos/gensler.svg" },
+  { id: 13, name: "Tishman Speyer", src: tismanSpeyer },
+  { id: 14, name: "AECOM", src: aecom },
+  { id: 15, name: "ASDSKY", src: asdsky },
+  { id: 16, name: "Gensler", src: gensler },
 ];
 
 const ROW_BOTTOM = [
-  { id: 17, name: "DLR Group", src: "/logos/dlr-group.webp" },
-  { id: 18, name: "Form Studio", src: "/logos/form.webp" },
-  { id: 19, name: "HOK", src: "/logos/hok.webp" },
-  { id: 20, name: "Gruen", src: "/logos/gruen.webp" },
-  { id: 21, name: "HED Design", src: "/logos/hed.webp" },
-  { id: 22, name: "Perkins & Will", src: "/logos/perkins-will.svg" },
-  { id: 23, name: "Huntsman Architectural", src: "/logos/huntsman.webp" },
-  { id: 24, name: "Johnson Fain", src: "/logos/johnson-fain.webp" },
-  { id: 25, name: "KDA", src: "/logos/kda.webp" },
-  { id: 26, name: "LPA Inc.", src: "/logos/lpa-inc.webp" },
-  { id: 27, name: "RIOS Inc.", src: "/logos/rios.webp" },
+  { id: 17, name: "DLR Group", src: dlrGroup },
+  { id: 18, name: "Form Studio", src: form },
+  { id: 19, name: "HOK", src: hok },
+  { id: 20, name: "Gruen", src: gruen },
+  { id: 21, name: "HED Design", src: hed },
+  { id: 22, name: "Perkins & Will", src: perkinsWill },
+  { id: 23, name: "Huntsman Architectural", src: huntsman },
+  { id: 24, name: "Johnson Fain", src: johnsonFain },
+  { id: 25, name: "KDA", src: kda },
+  { id: 26, name: "LPA Inc.", src: lpaInc },
+  { id: 27, name: "RIOS Inc.", src: rios },
   {
     id: 28,
     name: "Shlemmer Kamus Algaze",
-    src: "/logos/shlemmer-kamus-algaze.webp",
+    src: shlemmerKamusAlgaze,
   },
-  { id: 29, name: "SOM", src: "/logos/som.webp" },
-  { id: 30, name: "Studio One Eleven", src: "/logos/studio111.webp" },
-  { id: 31, name: "Ware Malcomb", src: "/logos/ware-malcomb.webp" },
-  { id: 32, name: "Area Architecture", src: "/logos/area.webp" },
+  { id: 29, name: "SOM", src: som },
+  { id: 30, name: "Studio One Eleven", src: studio111 },
+  { id: 31, name: "Ware Malcomb", src: wareMalcomb },
+  { id: 32, name: "Area Architecture", src: area },
 ];
 
 function LogoItem({ name, src }) {
